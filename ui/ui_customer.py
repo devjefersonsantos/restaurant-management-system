b93d85f111208223076f4854068bc02a89b7834a
Jeferson Santos <devjefersonsantos@gmail.com>
2024-04-10 16:40:15 -0300

    feat: add top bar

--- ui/ui_customer.py
+++ ui/ui_customer.py
@@ -1,5 +1,6 @@
 import customtkinter
 from database.db_login import DbLogin
+from utils.clear_frames import clear_frames
 
 class UiCustomer:
     def __init__(self, root: customtkinter.CTk, square_frame: customtkinter.CTk, token: str):
@@ -7,3 +8,40 @@ class UiCustomer:
         self.square_frame = square_frame
         self.__token = token
         DbLogin.verify_token(self.__token)
+
+        clear_frames(self.square_frame)
+        self.ui_customer()
+
+    def topbar(self):
+        topbar_frame = customtkinter.CTkFrame(master=self.square_frame,
+                                              width=1678, height=50,
+                                              corner_radius=0)
+        topbar_frame.place(x=0, y=0)
+        
+        topbar_label = customtkinter.CTkLabel(master=topbar_frame, 
+                                              font=("arial black", 25),
+                                              text_color="#ffffff", 
+                                              text="Customer")
+        topbar_label.place(x=20, y=5)
+
+        searchcustomers_entry = customtkinter.CTkEntry(master=topbar_frame,
+                                                       width=1227, height=35,
+                                                       placeholder_text="Search by name",
+                                                       font=("arial", 17), 
+                                                       fg_color="#EEEEEE", 
+                                                       border_color="#e3e3e3", 
+                                                       border_width=1)
+        searchcustomers_entry.place(x=174, y=8)
+
+        searchcustomers_button = customtkinter.CTkButton(master=topbar_frame,
+                                                         width=230, height=32,
+                                                         corner_radius=4,
+                                                         text_color="#ffffff",
+                                                         font=("arial", 15), 
+                                                         text="Search",
+                                                         fg_color="#407ecf", 
+                                                         hover_color="#6996d1")
+        searchcustomers_button.place(x=1425, y=9)
+
+    def ui_customer(self):
+        self.topbar()
--- ui/ui_home.py
+++ ui/ui_home.py
@@ -1,5 +1,6 @@
 import customtkinter
 from database.db_login import DbLogin
+from utils.clear_frames import clear_frames
 
 class UiHome:
     def __init__(self, root: customtkinter.CTk, square_frame: customtkinter.CTk, token: str):
@@ -7,3 +8,21 @@ class UiHome:
         self.square_frame = square_frame
         self.__token = token
         DbLogin.verify_token(self.__token)
+
+        clear_frames(self.square_frame)
+        self.ui_home()
+
+    def topbar(self):
+        topbar_frame = customtkinter.CTkFrame(master=self.square_frame,
+                                              width=1678, height=50,
+                                              corner_radius=0)
+        topbar_frame.place(x=0, y=0)
+        
+        topbar_label = customtkinter.CTkLabel(master=topbar_frame, 
+                                              font=("arial black", 25),
+                                              text_color="#ffffff", 
+                                              text="Home")
+        topbar_label.place(x=20, y=5)
+
+    def ui_home(self):
+        self.topbar()
