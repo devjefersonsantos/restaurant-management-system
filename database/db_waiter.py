12e73545b4acce92b6f5170246bc084dff7bdf61
Jeferson Santos <devjefersonsantos@gmail.com>
2024-04-22 14:02:00 -0300

    feat: add update and delete waiter

--- database/db_waiter.py
+++ database/db_waiter.py
@@ -1,6 +1,7 @@
 from database.database import Database
 from utils.empty_entries import empty_entries
 from tkinter import messagebox
+from database.db_login import DbLogin
 
 class DbWaiter(Database):
     def __init__(self, token):
@@ -8,7 +9,7 @@ class DbWaiter(Database):
         self.__token = token
 
     def create_waiter(self, name: str, cellphone: str) -> True:
-        __entry_items = {"name": name, "cellphone": cellphone}
+        __entry_items = {"name": name, "cellphone ": cellphone}
         
         if not empty_entries(**__entry_items):
             if self.connect_to_database():
@@ -36,6 +37,38 @@ class DbWaiter(Database):
                 messagebox.showerror(title="Read Waiters Error", message=error)
             else:
                 return self.result
+            finally:
+                self.cursor.close()
+                self.connection.close()
+
+    def update_waiter(self, new_name: str, new_cellphone: str, id_waiter: int) -> True:
+        __entry_items = {"name": new_name, "cellphone": new_cellphone}
+        
+        if not empty_entries(**__entry_items):
+            if self.connect_to_database():
+                try:
+                    self.cursor.execute("""UPDATE waiter
+                                        SET name = %s, cell_phone = %s
+                                        WHERE id_waiter = %s""", (new_name, new_cellphone, id_waiter))
+
+                    self.connection.commit()
+                except Exception as error:
+                    messagebox.showerror(title="Update Waiter Error", message=error)
+                else:
+                    messagebox.showinfo(title="Update Waiter", message=f"Waiter: {new_name}, updated successfully!")
+                    return True
+                finally:
+                    self.cursor.close()
+                    self.connection.close()
+
+    def delete_waiter(self, id_waiter: int) -> None:
+        if self.connect_to_database():
+            try:
+                self.cursor.execute("""DELETE FROM waiter
+                                    WHERE id_waiter = %s""", (id_waiter,))
+                self.connection.commit()
+            except Exception as error:
+                messagebox.showerror(title="Delete Waiter Error", message=error)
             finally:
                 self.cursor.close()
                 self.connection.close()
--- ui/ui_waiter.py
+++ ui/ui_waiter.py
@@ -20,49 +20,49 @@ class UiWaiter:
                                               corner_radius=0)
         topbar_frame.place(x=0, y=0)
         
-        topbar_label = customtkinter.CTkLabel(master=topbar_frame, 
-                                              font=("arial black", 25),
-                                              text_color="#ffffff", 
-                                              text="Waiter")
-        topbar_label.place(x=20, y=5)
+        self.topbar_label = customtkinter.CTkLabel(master=topbar_frame, 
+                                                   font=("arial black", 25),
+                                                   text_color="#ffffff", 
+                                                   text="Waiter")
+        self.topbar_label.place(x=20, y=5)
 
-        search_waiters_entry = customtkinter.CTkEntry(master=topbar_frame,
-                                                      width=1227, height=35,
-                                                      placeholder_text="Search by name",
-                                                      font=("arial", 17), 
-                                                      fg_color="#EEEEEE", 
-                                                      border_color="#e3e3e3", 
-                                                      border_width=1)
-        search_waiters_entry.place(x=174, y=8)
+        self.search_waiters_entry = customtkinter.CTkEntry(master=topbar_frame,
+                                                           width=1227, height=35,
+                                                           placeholder_text="Search by name",
+                                                           font=("arial", 17), 
+                                                           fg_color="#EEEEEE", 
+                                                           border_color="#e3e3e3", 
+                                                           border_width=1)
+        self.search_waiters_entry.place(x=174, y=8)
 
-        search_waiters_button = customtkinter.CTkButton(master=topbar_frame,
-                                                        width=230, height=32,
-                                                        corner_radius=4,
-                                                        text_color="#ffffff",
-                                                        font=("arial", 15), 
-                                                        text="Search",
-                                                        fg_color="#407ecf", 
-                                                        hover_color="#6996d1")
-        search_waiters_button.place(x=1425, y=9)
+        self.search_waiters_button = customtkinter.CTkButton(master=topbar_frame,
+                                                             width=230, height=32,
+                                                             corner_radius=4,
+                                                             text_color="#ffffff",
+                                                             font=("arial", 15), 
+                                                             text="Search",
+                                                             fg_color="#407ecf", 
+                                                             hover_color="#6996d1")
+        self.search_waiters_button.place(x=1425, y=9)
 
     def ui_waiter(self):
         clear_frames(self.square_frame)
 
         self.topbar()
 
-        create_waiter_frame = customtkinter.CTkFrame(master=self.square_frame,
-                                                     width=350, height=240,
-                                                     corner_radius=10,
-                                                     fg_color="#ffffff")
-        create_waiter_frame.place(x=10, y=58)
+        self.create_waiter_frame = customtkinter.CTkFrame(master=self.square_frame,
+                                                          width=350, height=240,
+                                                          corner_radius=10,
+                                                          fg_color="#ffffff")
+        self.create_waiter_frame.place(x=10, y=58)
 
-        waiter_name_label = customtkinter.CTkLabel(master=create_waiter_frame, 
+        waiter_name_label = customtkinter.CTkLabel(master=self.create_waiter_frame, 
                                                    font=("arial", 17), 
                                                    text_color="#383838", 
                                                    text="Name:")
         waiter_name_label.place(x=10, y=10)
 
-        self.waiter_name_entry = customtkinter.CTkEntry(master=create_waiter_frame,
+        self.waiter_name_entry = customtkinter.CTkEntry(master=self.create_waiter_frame,
                                                         width=330, height=35,
                                                         corner_radius=3, 
                                                         font=("arial", 17), 
@@ -70,13 +70,13 @@ class UiWaiter:
                                                         border_width=1)
         self.waiter_name_entry.place(x=10, y=45)
 
-        waiter_cellphone_label = customtkinter.CTkLabel(master=create_waiter_frame, 
+        waiter_cellphone_label = customtkinter.CTkLabel(master=self.create_waiter_frame, 
                                                         font=("arial", 17), 
                                                         text_color="#383838", 
                                                         text="Cell Phone:")
         waiter_cellphone_label.place(x=10, y=90)
 
-        self.waiter_cellphone_entry = customtkinter.CTkEntry(master=create_waiter_frame,
+        self.waiter_cellphone_entry = customtkinter.CTkEntry(master=self.create_waiter_frame,
                                                              width=330, height=35,
                                                              corner_radius=3, 
                                                              font=("arial", 17), 
@@ -84,59 +84,61 @@ class UiWaiter:
                                                              border_width=1)
         self.waiter_cellphone_entry.place(x=10, y=125)
 
-        create_waiter_button = customtkinter.CTkButton(master=create_waiter_frame,
-                                                       width=330, height=35,
-                                                       corner_radius=3,
-                                                       font=("arial", 15),
-                                                       text_color="#ffffff",
-                                                       text="Add Waiter",
-                                                       fg_color="#4bb34b", 
-                                                       hover_color="#7ebf7e",
-                                                       command=self.fn_create_waiter)
-        create_waiter_button.place(x=10, y=185)
+        self.create_waiter_button = customtkinter.CTkButton(master=self.create_waiter_frame,
+                                                            width=330, height=35,
+                                                            corner_radius=3,
+                                                            font=("arial", 15),
+                                                            text_color="#ffffff",
+                                                            text="Add Waiter",
+                                                            fg_color="#4bb34b", 
+                                                            hover_color="#7ebf7e",
+                                                            command=self.fn_create_waiter)
+        self.create_waiter_button.place(x=10, y=185)
 
-        update_waiter_frame = customtkinter.CTkFrame(master=self.square_frame,
-                                                     width=350, height=100,
-                                                     corner_radius=10,
-                                                     fg_color="#ffffff")
-        update_waiter_frame.place(x=10, y=680)
+        self.update_waiter_frame = customtkinter.CTkFrame(master=self.square_frame,
+                                                          width=350, height=100,
+                                                          corner_radius=10,
+                                                          fg_color="#ffffff")
+        self.update_waiter_frame.place(x=10, y=680)
 
-        update_waiter_label = customtkinter.CTkLabel(master=update_waiter_frame, 
+        update_waiter_label = customtkinter.CTkLabel(master=self.update_waiter_frame, 
                                                      font=("arial", 17), 
                                                      text_color="#383838", 
                                                      text="Update selected waiter:")
         update_waiter_label.place(x=10, y=10)
 
-        update_waiter_button = customtkinter.CTkButton(master=update_waiter_frame,
+        update_waiter_button = customtkinter.CTkButton(master=self.update_waiter_frame,
                                                        width=330, height=35,
                                                        corner_radius=3,
                                                        font=("arial", 15),
                                                        text_color="#ffffff",
                                                        text="Update Waiter",
                                                        fg_color="#ec971f", 
-                                                       hover_color="#f0b35d")
+                                                       hover_color="#f0b35d",
+                                                       command=self.ui_update_waiter)
         update_waiter_button.place(x=10, y=45)
 
-        delete_waiter_frame = customtkinter.CTkFrame(master=self.square_frame,
-                                                     width=350, height=100,
-                                                     corner_radius=10,
-                                                     fg_color="#ffffff")
-        delete_waiter_frame.place(x=10, y=795)
+        self.delete_waiter_frame = customtkinter.CTkFrame(master=self.square_frame,
+                                                          width=350, height=100,
+                                                          corner_radius=10,
+                                                          fg_color="#ffffff")
+        self.delete_waiter_frame.place(x=10, y=795)
 
-        delete_waiter_label = customtkinter.CTkLabel(master=delete_waiter_frame, 
+        delete_waiter_label = customtkinter.CTkLabel(master=self.delete_waiter_frame, 
                                                      font=("arial", 17), 
                                                      text_color="#383838", 
                                                      text="Delete selected waiter:")
         delete_waiter_label.place(x=10, y=10)
 
-        delete_waiter_button = customtkinter.CTkButton(master=delete_waiter_frame,
+        delete_waiter_button = customtkinter.CTkButton(master=self.delete_waiter_frame,
                                                        width=330, height=35,
                                                        corner_radius=3,
                                                        font=("arial", 15),
                                                        text_color="#ffffff",
                                                        text="Delete Waiter",
                                                        fg_color="#d54a49", 
-                                                       hover_color="#d1706f")
+                                                       hover_color="#d1706f",
+                                                       command=self.fn_delete_waiter)
         delete_waiter_button.place(x=10, y=45)
 
         style = tkinter.ttk.Style()
@@ -151,15 +153,15 @@ class UiWaiter:
                                                     show="headings")
         self.waiter_treeview.place(x=370, y=58)
 
-        self.waiter_treeview.heading("#1", text="  ID", anchor="w")
-        self.waiter_treeview.heading("#2", text=" name", anchor="w")
-        self.waiter_treeview.heading("#3", text=" cell phone", anchor="w")
-        self.waiter_treeview.heading("#4", text=" registration date", anchor="w")
+        self.waiter_treeview.heading("#1", text="ID", anchor="center")
+        self.waiter_treeview.heading("#2", text="name", anchor="center")
+        self.waiter_treeview.heading("#3", text="cell phone", anchor="center")
+        self.waiter_treeview.heading("#4", text="registration date", anchor="center")
 
         self.waiter_treeview.column("#1", minwidth=100, width=150, anchor="center")
-        self.waiter_treeview.column("#2", minwidth=200, width=350, anchor="w")
-        self.waiter_treeview.column("#3", minwidth=250, width=375, anchor="w")
-        self.waiter_treeview.column("#4", minwidth=300, width=425, anchor="w")
+        self.waiter_treeview.column("#2", minwidth=200, width=350, anchor="center")
+        self.waiter_treeview.column("#3", minwidth=250, width=375, anchor="center")
+        self.waiter_treeview.column("#4", minwidth=300, width=425, anchor="center")
 
         treeview_scrollbar = tkinter.Scrollbar(self.square_frame, orient=tkinter.VERTICAL, command=self.waiter_treeview.yview)
         self.waiter_treeview.configure(yscroll=treeview_scrollbar.set)
@@ -167,12 +169,43 @@ class UiWaiter:
 
         self.fn_read_waiters()
 
+    def ui_update_waiter(self):
+        self.data = self.selected_row()
+        if not self.data:
+            return
+        
+        self.topbar_label.configure(text="Update Waiter")
+        self.search_waiters_entry.destroy()
+        self.search_waiters_button.destroy()
+        
+        self.update_waiter_frame.destroy()
+        self.delete_waiter_frame.destroy()
+
+        self.waiter_name_entry.delete(0, "end")
+        self.waiter_cellphone_entry.delete(0, "end")
+
+        self.create_waiter_frame.configure(height=300)
+        self.create_waiter_button.configure(text="Save Changes", 
+                                            command=self.fn_update_waiter)
+
+        self.cancel_button = customtkinter.CTkButton(master=self.create_waiter_frame,
+                                                     width=330, height=35,
+                                                     corner_radius=3,
+                                                     font=("arial", 15),
+                                                     text_color="#ffffff",
+                                                     text="Cancel",
+                                                     fg_color="#5c5c5c", 
+                                                     hover_color="#6e6e6e",
+                                                     command=self.ui_waiter)
+        self.cancel_button.place(x=10, y=240)
+
+        self.waiter_name_entry.insert(0, self.data[1])
+        self.waiter_cellphone_entry.insert(0, self.data[2])
+
     def fn_create_waiter(self):
         if DbWaiter(token=self.__token).create_waiter(name=self.waiter_name_entry.get(),
                                                       cellphone=self.waiter_cellphone_entry.get()):
-            self.waiter_name_entry.delete(0, "end")
-            self.waiter_cellphone_entry.delete(0, "end")
-            self.root.focus()
+            self.clear_entries()
             self.fn_read_waiters()
 
     def fn_read_waiters(self):
@@ -188,3 +221,39 @@ class UiWaiter:
         for i in __all_waiters:
             tag = "hexgray" if tag == "hexwhite" else "hexwhite"
             self.waiter_treeview.insert("", "end", values=i, tags=tag)
+    
+    def fn_update_waiter(self):
+        self.data = self.selected_row()
+        if not self.data:
+            return
+        
+        updated_waiter = DbWaiter(self.__token).update_waiter(new_name=self.waiter_name_entry.get(),
+                                                              new_cellphone=self.waiter_cellphone_entry.get(),
+                                                              id_waiter=self.data[0])
+        
+        if updated_waiter:
+            self.ui_waiter()
+
+    def fn_delete_waiter(self):
+        self.data = self.selected_row()
+        if not self.data:
+            return
+        
+        message = f"Are you sure you want to delete\nthis waiter? {self.data[1]}."
+        if tkinter.messagebox.askyesno(title="Delete Waiter", 
+                                       message=message, 
+                                       icon=tkinter.messagebox.WARNING) == True:
+            DbWaiter(self.__token).delete_waiter(id_waiter=self.data[0])
+            self.fn_read_waiters()
+
+    def selected_row(self) -> tuple:
+        try:
+            selected_waiter = self.waiter_treeview.item(self.waiter_treeview.selection()[0], "values")
+            return selected_waiter
+        except IndexError:
+            tkinter.messagebox.showerror(title=None, message="Please select a waiter")
+
+    def clear_entries(self):
+        self.waiter_name_entry.delete(0, "end")
+        self.waiter_cellphone_entry.delete(0, "end")
+        self.root.focus()
